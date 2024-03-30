2003f31bcab9eacbb006cf548b5f4c5379c61b4e
Asif Ahammad <asifahammed359@gmail.com>
2024-03-30 23:09:53 +0600

    add : besic print

---
+++ Besic_Print/BesicPrint.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hi");
+    printf("I am a programmer");
+
+    printf("\n"); // for new line
+
+    printf("Hi\n");
+    printf("I am a programmer");
+
+    printf("\n");
+
+    printf("ahfouw238498@#(!!)!~_+$");
+
+    printf("\n");
+
+    // printf("%"); not valid
+    printf("%%"); // valid
+
+    printf("\n");
+
+    // printf("\"); not valid
+    printf("\\"); // valid
+
+    printf("\n");
+
+    printf("hey tab(\t) Mama"); //\t for tab 
+
+    return 0;
+}
