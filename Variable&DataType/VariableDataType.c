d58865e7618e308edb51af4a034240a17fc2dd59
Asif Ahammad <asifahammed359@gmail.com>
2024-03-31 00:34:23 +0600

    add: comment

--- Variable&DataType/VariableDataType.c
+++ Variable&DataType/VariableDataType.c
@@ -6,10 +6,11 @@ int main(int argc, char const *argv[])
     /*
 
     Data type
-    1. Integer int 
-    2. Float float 
-    3. Character char
-    4. Boolean bool
+    1. Integer int      4 byte
+    2. Float float      4 byte
+    3. Character char   1 byte
+    4. Boolean bool     1 byte
+    5. Double double    8 byte
 
     Variable
     Variable name can be anything that Start with letter or _ (not special key word example: int , main , printf etc)
@@ -21,13 +22,24 @@ int main(int argc, char const *argv[])
 
     */
 
-    int Number = 10;
-    float Number2 = 5.10;
-    char character = 'A';
-    bool isRight = true;
+    int Number;  // Dealer
+    Number = 10; // value assign in Variable (assign)
+
+    float Number2 = 5.101112345; // Dealer + (assign) value assign in Variable = initialize
+
+    char character = 'A'; // initialize
+    bool isRight = true;  // initialize
+
+    // Format specifier
+    /*
+    int     - %d 
+    float   - %f
+    char    - %c 
+    double  - %lf
+    */
 
     printf("Integer Number : %d\n", Number);
-    printf("Float Number   : %f\n", Number2);
+    printf("Float Number   : %0.4f\n", Number2);
     printf("Character      : %c\n", character);
 
     if (isRight == true)
