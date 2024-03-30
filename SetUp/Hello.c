73f2df3b4bd97c788be8629668f86b51eb340914
Asif Ahammad <asifahammed359@gmail.com>
2024-03-30 18:07:15 +0600

    loop in floate type increment

--- SetUp/Hello.c
+++ SetUp/Hello.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    int i ;
-    printf("Enter i : ");
-    scanf("%d",&i);
-    printf("hello world %d" ,i);
+    float i;
+    int n;
+    printf("Enter N : ");
+    scanf("%d", &n);
+    for (i = 0.0; i < n+1; i=i+0.01)
+    {
+        printf("%f \n", i);
+    }
+
+    return 0;
 }
