ef9bd5daea0051b821eb4cf69ddbbe22857f35bc
Asif Ahammad <asifahammed359@gmail.com>
2024-04-01 00:04:01 +0600

    Add: statement

--- conditionalStatement/conditional.c
+++ conditionalStatement/conditional.c
@@ -20,7 +20,18 @@ int main(int argc, char const *argv[])
     }
     else
     {
-         printf("Null");
+        printf("Null");
+    }
+
+    int x = 0;
+
+    if (x = 1)
+    {
+        printf("hi");
+    }
+    else
+    {
+        printf("how are u");
     }
 
     return 0;
